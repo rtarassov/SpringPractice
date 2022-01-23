ad474964062fa755c4b2e627bcef2a79838f49c5
Richard <richard.tarassov@hotmail.com>
2022-01-23 10:08:56 +0200

    [change] - exercises with controller

--- src/main/java/com/sda/javaee9spring/controller/FirstController.java
+++ src/main/java/com/sda/javaee9spring/controller/FirstController.java
@@ -1,5 +1,6 @@
 package com.sda.javaee9spring.controller;
 
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
+//@Value
 public class FirstController {
 //    The same as @Slf4j annotation above.
 //    private static final Logger log = LoggerFactory.getLogger(FirstController.class);
@@ -15,6 +17,18 @@ public class FirstController {
     public String firstPage() {
         log.info("firstPage() method called!!!");
 //        looking for html page inside resources/templates
+//        Spring please use page: resources/templates/home-page.html
         return "home-page";
+    }
+    @GetMapping("/my-second-page")
+    public String secondPage() {
+        log.info("secondPage() method called!");
+        return "second-page";
+    }
+
+    @GetMapping("/third-page")
+    public String thirdPage() {
+        log.info("thirdPage() method called!");
+        return "pages/third-page";
     }
 }
