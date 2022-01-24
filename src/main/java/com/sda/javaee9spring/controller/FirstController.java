e82659d4026d6fecbcdf198cbfde035921e77fa6
Richard <richard.tarassov@hotmail.com>
2022-01-24 17:35:39 +0200

    [change] - sending query parameters to endpoint - default parameters values

--- src/main/java/com/sda/javaee9spring/controller/FirstController.java
+++ src/main/java/com/sda/javaee9spring/controller/FirstController.java
@@ -1,49 +1,60 @@
 package com.sda.javaee9spring.controller;
 
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
-//@Value
+@RequestMapping("/first")
 public class FirstController {
-//    The same as @Slf4j annotation above.
-//    private static final Logger log = LoggerFactory.getLogger(FirstController.class);
+
+    // the same as @Slf4j
+    // private static final Logger log = LoggerFactory.getLogger(FirstController.class);
+
     @GetMapping({"/my-first-page", "/"})
     public String firstPage() {
         log.info("firstPage() method called!!!");
-//        looking for html page inside resources/templates
-//        Spring please use page: resources/templates/home-page.html
+        // looking for html page inside resources/templates
+        // Spring please use page: resources/templates/home-page.html
         return "home-page";
     }
+
     @GetMapping("/my-second-page")
     public String secondPage() {
-        log.info("secondPage() method called!");
+        log.info("secondPage() method called!!!");
+        //looking for html page inside resources/templates
         return "second-page";
     }
 
-//    @GetMapping("/third-page")
-    @GetMapping(value ={"/third-page", "third-too"})
+    //    @GetMapping("/third-page")
+//    @GetMapping(value = "/third-page")
+//    @GetMapping(value = {"/third-page"}, params = {})
+    @GetMapping(value = {"/third-page", "/third-too", "/abcd"})
     public String thirdPage() {
-        log.info("thirdPage() method called!");
+        log.info("thirdPage() method was called!!!");
         return "pages/third-page";
     }
 
-//    /my-name?name=richard&surname=tarassov
-//    name param with value richard
-//    surname param with value tarassov
+    // /my-name?name=mariusz&surname=pastuszka
+    // name param with value mariusz
+    // surname param with value pastuszka
     @GetMapping("/my-name")
-    public String myName(@RequestParam(value = "name", defaultValue = "Richard") String myName,
-                         @RequestParam(value = "surname", defaultValue = "Tarassov") String mySurname) {
-        log.info("myName() called");
-        log.info("my name is: [{}] and my surname is: [{}]", myName, mySurname);
-//        log.info(String.format("my name is: [%s] and my surname is: [%s]", myName, mySurname));
+    public String myName(@RequestParam(value = "name", defaultValue = "Jan") String myFirstName,
+                         @RequestParam(value = "surname", defaultValue = "Kowalski") String mySurname) {
+        log.info("myName() method was called!!!");
+        log.info("my name is: [{}] and my surname is: [{}]", myFirstName, mySurname);
+        log.info(String.format("my name is: [%s] and my surname is: [%s]", myFirstName, mySurname));
 
+        return "";
+    }
+
+    @PostMapping("/my-first-post")
+    public String myFirstOtherThanGetHttpMethod() {
+        log.info("myFirstOtherThanGetHttpMethod() was called");
         return "";
     }
 }
