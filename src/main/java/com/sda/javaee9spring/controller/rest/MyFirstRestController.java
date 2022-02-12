d3e1cf671142785e22f0af13eab18018c1f4ec6a
Richard <richard.tarassov@hotmail.com>
2022-02-12 11:42:32 +0200

    [change] - JSON with nested content

--- src/main/java/com/sda/javaee9spring/controller/rest/MyFirstRestController.java
+++ src/main/java/com/sda/javaee9spring/controller/rest/MyFirstRestController.java
@@ -1,10 +1,14 @@
 package com.sda.javaee9spring.controller.rest;
 
+import com.sda.javaee9spring.entity.Child;
+import com.sda.javaee9spring.entity.Mother;
 import com.sda.javaee9spring.entity.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -16,5 +20,19 @@ public class MyFirstRestController {
     public Person onePerson() {
         log.info("onePerson() method called!");
         return new Person("Richard", "T", 22);
+    }
+
+    @GetMapping("/persons-array")
+    public Person[] personsArray() {
+        return new Person[] {
+                new Person("Kasia", "P.", 15),
+                new Person("Marek", "Nowak", 35)
+        };
+    }
+
+    @GetMapping("/mother")
+    public Mother motherAndChild() {
+        return new Mother("Ewelina",
+                new Child("Frank", "male"));
     }
 }
