117d1e3d1a7603fc19079ed1225c9bb652d7b107
Richard <richard.tarassov@hotmail.com>
2022-02-13 13:45:25 +0200

    [add] - script for initialization database with one endpoint for reading all PersonEntity objects

--- src/main/java/com/sda/javaee9spring/service/RealPersonService.java
+++ src/main/java/com/sda/javaee9spring/service/RealPersonService.java
@@ -1,14 +1,27 @@
 package com.sda.javaee9spring.service;
 
+import com.sda.javaee9spring.entity.PersonEntity;
 import com.sda.javaee9spring.repository.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@Slf4j
 public class RealPersonService {
 
     private final PersonRepository personRepository;
 
     public RealPersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
+    }
+
+    public List<PersonEntity> readAllPersonEntities() {
+        log.info("trying to read all persons entities...");
+
+        var result = personRepository.findAll();
+        log.info("'person entities read from DB: " + result);
+        return result;
     }
 }
